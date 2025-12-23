8552b7e8caa41bd99bc3aaf7b110cbd47bc6c186
Amitanand0123 <amitanand.nd.in@gmail.com>
2025-12-23 17:37:44 +0530

    email setup

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,13 +4,16 @@ const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
+            type: 'OAuth2',
             user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            refreshToken: process.env.GOOGLE_REFRESH_TOKEN
+        }
     });
 
     const mailOptions = {
-        from: `"Sol9x Admin" <${process.env.EMAIL_USER}>`,
+        from: `"Sol9x Support" <${process.env.EMAIL_USER}>`,
         to: options.email,
         subject: options.subject,
         html: options.message,
